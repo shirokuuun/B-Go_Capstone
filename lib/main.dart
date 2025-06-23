e51ed941a03ea96db46cbbbfc504ab240ec36640
Marcus Neo Rangel <marcusneorangel23@gmail.com>
2025-06-23 19:27:55 +0800

    added a google map on the home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
-import 'package:b_go/auth/main_page.dart';
 import 'package:b_go/pages/get_started.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-//import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    //options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+  await dotenv.load();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 

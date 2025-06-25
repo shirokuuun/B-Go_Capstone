7d6faf434966a241b8bb67a17bd6d6745dff5a5e
rensonpena123 <rensonpenag@gmail.com>
2025-06-25 23:53:45 +0800

    fixed the display of Name and km, and the sorting of the km changed to ascending order

--- lib/pages/conductor/route_service.dart
+++ lib/pages/conductor/route_service.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RouteService {
+
+  // get ROUTE
   static Future<String> fetchRoutePlaceName() async {
     final doc = await FirebaseFirestore.instance
         .collection('Destinations')
@@ -22,5 +24,31 @@ class RouteService {
     } else {
       return 'Route not found';
     }
+  }
+
+  // get PLACES
+  static Future<List<Map<String, dynamic>>> fetchPlaces() async {
+    var snapshot = await FirebaseFirestore.instance
+        .collection('Destinations')
+        .doc('Batangas')
+        .collection('Place')
+        .get();
+
+    List<Map<String, dynamic>> places = snapshot.docs.map((doc) {
+      final data = doc.data();
+      return {
+        'name': data['Name']?.toString() ?? doc.id,
+        'km': data['km'], // keep as number if possible
+      };
+    }).toList();
+
+    // Sort by 'km', treating null or missing as a high value (so they go last)
+    places.sort((a, b) {
+      num akm = a['km'] ?? double.infinity;
+      num bkm = b['km'] ?? double.infinity;
+      return akm.compareTo(bkm);
+    });
+
+    return places;
   }
 }

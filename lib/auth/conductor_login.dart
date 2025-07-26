d3eb2adb551702b2ab010b2cd49ebc9a54af18df
Marcus Neo Rangel <marcusneorangel23@gmail.com>
2025-07-26 23:41:34 +0800

    Tweaked a little for the conductor login page. added a button for the reference back to the original login page

--- lib/auth/conductor_login.dart
+++ lib/auth/conductor_login.dart
@@ -5,6 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:b_go/pages/conductor/route_service.dart';
 import 'package:b_go/responsiveness/responsive_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:math' as math;
 
 class ConductorLogin extends StatefulWidget {
   const ConductorLogin({super.key});
@@ -92,21 +93,39 @@ class _ConductorLoginState extends State<ConductorLogin> {
     Responsive responsive = Responsive(context);
     return Scaffold(
       backgroundColor: const Color(0xFFE5E9F0),
-      body: SafeArea(
-        bottom: false,
-        child: Column(
-          children: [
-            const SizedBox(height: 24),
-            Center(
-              child: Image.asset(
-                'assets/batrasco-logo.png',
-                width: 150,
-                fit: BoxFit.contain,
-              ),
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.38,
+            decoration: BoxDecoration(
+              color: Color(0xFFE5E9F0),
             ),
-            const SizedBox(height: 16),
-            Expanded(
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                double maxLogoWidth = 400.0;
+                double logoWidth = 150;
+                math.min(constraints.maxWidth * 0.4, maxLogoWidth);
+                return Transform.translate(
+                  offset: Offset(0, -20),
+                  child: Center(
+                    child: Image.asset(
+                      'assets/batrasco-logo.png',
+                      width: logoWidth,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+
+          // --- LOGIN BOX ---
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SingleChildScrollView(
               child: Container(
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.24),
                 width: double.infinity,
                 decoration: const BoxDecoration(
                   color: Colors.white,
@@ -119,164 +138,159 @@ class _ConductorLoginState extends State<ConductorLogin> {
                     ),
                   ],
                 ),
-                padding: EdgeInsets.only(
-                  top: responsive.height * 0.05,
-                  left: responsive.width * 0.07,
-                  right: responsive.width * 0.07,
-                  bottom: responsive.height * 0.20,
-                ),
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const SizedBox(height: 20),
-                      Center(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Login",
-                              style: GoogleFonts.outfit(
-                                fontSize: 45,
-                              ),
-                            ),
-                            Text(
-                              "Welcome Back, Conductor!",
-                              style: GoogleFonts.outfit(
-                                fontSize: 20,
-                                color: const Color.fromARGB(255, 0, 0, 0),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 40),
-                      // Email
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: const Color(0xFFE5E9F0),
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 20.0),
-                            child: TextField(
-                              controller: _emailController,
-                              style: GoogleFonts.outfit(color: Colors.black),
-                              decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "Email",
-                                hintStyle: GoogleFonts.outfit(
-                                    color: Colors.black54,
-                                    fontWeight: FontWeight.w700),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 30),
-                      // Password
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: const Color(0xFFE5E9F0),
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 20.0),
-                            child: TextField(
-                              controller: _passwordController,
-                              obscureText: true,
-                              style: GoogleFonts.outfit(color: Colors.black),
-                              decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "Password",
-                                hintStyle: GoogleFonts.outfit(
-                                    color: Colors.black54,
-                                    fontWeight: FontWeight.w700),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      // Error message
-                      if (_errorMessage != null) ...[
-                        const SizedBox(height: 12),
-                        Center(
-                          child: Text(
-                            _errorMessage!,
-                            style: const TextStyle(color: Colors.red),
-                          ),
-                        ),
-                      ],
-                      const SizedBox(height: 20),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                        child: SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: const Color(0xFF1D2B53),
-                              padding: const EdgeInsets.all(20),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12),
-                              ),
-                            ),
-                            onPressed: () {
-                              _login();
-                            },
-                            child: Text(
-                              'Login',
-                              style: GoogleFonts.outfit(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-
-                      SizedBox(height: 25),
-
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                padding: const EdgeInsets.only(left: 28, right: 28, top: 50, bottom: 150),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // --- LOGIN BOX ---
+                    Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Text('Not a conductor?',
-                              style: GoogleFonts.outfit(
-                                  fontWeight: FontWeight.w500)),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pushNamed(context, '/login');
-                            },
-                            style: TextButton.styleFrom(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 2,
-                                  vertical: 4), // Increase tap area
-                              minimumSize:
-                                  Size(0, 0), // Remove min size if needed
-                              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          Text(
+                            "Login",
+                            style: GoogleFonts.outfit(
+                              fontSize: 45,
                             ),
-                            child: Text(
-                              "Sign In",
-                              style: GoogleFonts.outfit(
-                                color: Color(0xFF2397f3),
-                              ),
+                          ),
+                          Text(
+                            "Welcome Back, Conductor!",
+                            style: GoogleFonts.outfit(
+                              fontSize: 20,
+                              color: const Color.fromARGB(255, 0, 0, 0),
                             ),
                           ),
                         ],
-                      )
+                      ),
+                    ),
+
+                    SizedBox(height: 40),
+
+                    // Email
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFE5E9F0),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20.0),
+                          child: TextField(
+                            controller: _emailController,
+                            style: GoogleFonts.outfit(color: Colors.black),
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Email",
+                              hintStyle: GoogleFonts.outfit(
+                                  color: Colors.black54,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    // Password
+                    SizedBox(height: 30),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFE5E9F0),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20.0),
+                          child: TextField(
+                            controller: _passwordController,
+                            obscureText: true,
+                            style: GoogleFonts.outfit(color: Colors.black),
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Password",
+                              hintStyle: GoogleFonts.outfit(
+                                  color: Colors.black54,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+
+                    // Error message
+                    if (_errorMessage != null) ...[
+                      const SizedBox(height: 12),
+                      Center(
+                        child: Text(
+                          _errorMessage!,
+                          style: const TextStyle(color: Colors.red),
+                        ),
+                      ),
                     ],
-                  ),
+
+                    // Sign in button
+                    SizedBox(height: 40),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFF1D2B53),
+                            padding: EdgeInsets.all(20),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          onPressed: () {
+                            _login();
+                          },
+                          child: Text(
+                            'Login',
+                            style: GoogleFonts.outfit(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+
+                    // Register section
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Not a conductor?',
+                            style: GoogleFonts.outfit(
+                                fontWeight: FontWeight.w500)),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/login');
+                          },
+                          style: TextButton.styleFrom(
+                            padding: EdgeInsets.symmetric(horizontal: 2, vertical: 4),
+                            minimumSize: Size(0, 0),
+                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          ),
+                          child: Text(
+                            "Sign In",
+                            style: GoogleFonts.outfit(
+                              color: Color(0xFF2397f3),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
